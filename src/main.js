546606fd73f4ffd8b72c1943412b38bd230147f8
abdounoe <53197308+abdounoe@users.noreply.github.com>
2023-03-23 20:17:56 +0000

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/PNS6Dok2Srb-01Nh/scene.splinecode');
